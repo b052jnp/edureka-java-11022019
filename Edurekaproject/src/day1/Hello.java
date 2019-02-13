087ea2347602a5678e8637105e5533088a968c38
Jayakrishnan <Jayakrishnan@DESKTOP-NJ1UE96>
2019-02-13 20:09:59 +0530

    my first commit

---
+++ Edurekaproject/src/day1/Hello.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		int number = 100;
+		char name = 'J'; 
+		boolean flag= true;
+		String str ="sjhjkhadjskhjksah";
+
+	System.out.println(" Hello"+  name +number + flag +str);
+	}
+
+}
